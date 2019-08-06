a9de5a80bc21fb4d3f205655169fba86a252a533
gauldivic <acb9805@rit.edu>
2019-08-06 17:36:39 -0400

    First Commit

---
+++ src/build/precache-manifest.e0e496fc01fa14a258aa0e5bcc45e1c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "09d4f946e7db7b39153b",
+    "url": "/static/js/main.739c2129.chunk.js"
+  },
+  {
+    "revision": "ee7cce24612f0c2c6a80",
+    "url": "/static/js/2.b3a241f9.chunk.js"
+  },
+  {
+    "revision": "09d4f946e7db7b39153b",
+    "url": "/static/css/main.186e4923.chunk.css"
+  },
+  {
+    "revision": "88ed14bd97f58d2dbca7fcf6f1e909e7",
+    "url": "/index.html"
+  }
+];
